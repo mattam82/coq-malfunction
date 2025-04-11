9db80882cb633bb32f97d86d678a4e0926dbdd5d
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-04-11 02:36:28 +0200

    Export new Tactics

--- plugin/plugin-bootstrap/Extraction.v
+++ plugin/plugin-bootstrap/Extraction.v
@@ -1,4 +1,9 @@
+(** The plugin loader *)
 From VerifiedExtraction Require Export Loader.
-From Malfunction Require Export PrintMli.
+(* From Malfunction Require Export PrintMli. *)
 
+(** Bindings to primitive type implementations. *)
 From VerifiedExtraction Require Export PrimInt63 PrimFloat PrimArray PrimString RocqMsgFFI.
+
+(** Ltac2 tactics using the erasure cast for reflexive proofs. *)
+From VerifiedExtraction Require Export Tactics.
